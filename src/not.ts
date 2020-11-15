751bb628347ddc11dd89b4d61714f0769f730dee
Andrew Huth <andrew@huth.me>
2020-11-14 17:30:12 -0800

    Simplify the `not` gate
    The 'input' wire wasn't really an input... I realized that the only purpose of that "wire" was to hold a value, which we can achieve with a variable.

--- src/not.ts
+++ src/not.ts
@@ -1,11 +1,11 @@
 import { invert, Signal } from './signal';
-import makeWire, { Wire } from './wire';
+import { Wire } from './wire';
 
 export default function* not(initialInput: Signal = 0): Wire {
-  const wire = makeWire(initialInput);
-  let value: Signal = 1;
+  let input = initialInput;
 
   while (true) {
-    value = yield invert(wire.next(value).value);
+    const nextInput = yield invert(input);
+    input = nextInput ?? input;
   }
 }
